a25e4e80170688268cabe1b418887aad3bc7170c
ndm767 <5031736+ndm767@users.noreply.github.com>
2021-03-12 01:02:13 -0500

    added command line arguments

--- main.cpp
+++ main.cpp
@@ -21,31 +21,49 @@ Brainfuck instructions taken from https://en.wikipedia.org/wiki/Brainfuck
 
 /*
 options:
--h help
--v version
 -d debug
 -o output
 -c config
 */
 
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
 int main(int argc, char* argv[]){
-    bool printVersion;
-    bool printHelp;
     bool debug;
     bool output;
     bool config;
     char* args[3]; //0 - file to compile, 1 - output file, 2 - config file
     if(argc < 2){
-        printf("Not enough arguments! Usage: %s file [-h, -v, -d, -o output, -c config]", argv[0]);
+        printf("Not enough arguments! Usage: %s file [-d, -o output, -c config]", argv[0]);
         return 0;
     }
 
     for(int i = 1; i<argc; i++){
-        
+        //check if argument is a command
+        if(argv[i][0] == '-'){
+            if(argv[i][1] == 'd'){
+                printf("debug\n");
+                debug = true;
+            }else if(argv[i][1] == 'o'){
+                printf("output\n");
+                output = true;
+                i++;
+                args[1] = argv[i];
+            }else if(argv[i][1] == 'c'){
+                printf("config\n");
+                config = true;
+                i++;
+                args[2] = argv[i];
+            }
+        }else{
+            //otherwise it must be the file
+            args[0] = argv[i];
+        }
     }
+    printf("file: %s\n", args[0]);
+    if(output) printf("output file: %s\n", args[1]);
+    if(config) printf("config file: %s\n", args[2]);
 
     return 0;
 }
